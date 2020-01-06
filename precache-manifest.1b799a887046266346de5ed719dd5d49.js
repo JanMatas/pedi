2b6d85670de5dfa48461fbbca41f379d1172c0a6
Jan Matas <janci5243@gmail.com>
2020-01-06 01:04:38 +0100

    Updates

---
+++ precache-manifest.1b799a887046266346de5ed719dd5d49.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "952e5da9ba7266b089a2b30ea15cb361",
+    "url": "/pedi/index.html"
+  },
+  {
+    "revision": "b82deeaccb13988a398c",
+    "url": "/pedi/static/css/main.ab545f90.chunk.css"
+  },
+  {
+    "revision": "9c3c5a147a06257419f8",
+    "url": "/pedi/static/js/2.661a371d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/pedi/static/js/2.661a371d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b82deeaccb13988a398c",
+    "url": "/pedi/static/js/main.4f895cc2.chunk.js"
+  },
+  {
+    "revision": "ec0187e877b229d7ca8b",
+    "url": "/pedi/static/js/runtime-main.84bfad1d.js"
+  }
+]);

3ca569a8fe8fa5b30cab87601e31fbea0cc698e1
Jan Matas <janci5243@gmail.com>
2020-01-06 19:02:27 +0100

    Updates

---
+++ precache-manifest.d29dc3322349da7a9224f9ca0028d317.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27a849e6e10d8f3f382ae2e64177499c",
+    "url": "/pedi/index.html"
+  },
+  {
+    "revision": "5d3158f7cf4f1076e524",
+    "url": "/pedi/static/css/main.30603f2d.chunk.css"
+  },
+  {
+    "revision": "83b86a2d0278571211fc",
+    "url": "/pedi/static/js/2.e84fe44b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/pedi/static/js/2.e84fe44b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5d3158f7cf4f1076e524",
+    "url": "/pedi/static/js/main.5b650420.chunk.js"
+  },
+  {
+    "revision": "ec0187e877b229d7ca8b",
+    "url": "/pedi/static/js/runtime-main.84bfad1d.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/pedi/precache-manifest.300430209f3e1b46489ca45cf74995d5.js"
+  "/pedi/precache-manifest.d29dc3322349da7a9224f9ca0028d317.js"
 );
 
 self.addEventListener('message', (event) => {
